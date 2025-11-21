6427129e84e558e55969b51b04769ce2948da64b
Cristhyan Dal Bosco <cristhyandalbosco@gmail.com>
2025-11-21 19:30:07 -0300

    create ex 009 e ex 010

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,8 +1,7 @@
-var vel = 70.5
+//estamos executando o js no node pelo terminal, por isso precisamos abrir com console.log, diferente do navegador que iriamos mostrar com display.document.write
+var vel = 50.5
 console.log(`A velocidade do seu carro é ${vel}km/h`)
-    if (vel > 60) {
+    if (vel > 60) { //condição composta
         console.log(`Você ultrapassou a velocidade máxima permitida. MULTADO!`)
-    } else {
-        console.log(`Parabéns, seus filhos ficarão felizes de você ter chegado inteiro em casa!`)
     }
 console.log(`Dirija sempre usando cinto de segurança!`)
