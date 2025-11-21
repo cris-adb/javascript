dc8eada6ed5237c72b1081a386764d9a771e6f76
Cristhyan Dal Bosco <cristhyandalbosco@gmail.com>
2025-11-21 19:21:32 -0300

    create ex008

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+    if (vel > 60) {
+        console.log(`Você ultrapassou a velocidade máxima permitida. MULTADO!`)
+    } else {
+        console.log(`Parabéns, seus filhos ficarão felizes de você ter chegado inteiro em casa!`)
+    }
+console.log(`Dirija sempre usando cinto de segurança!`)
