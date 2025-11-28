bf8b129ec8e45dbeb860db43679048dbab3d29c9
Cristhyan Dal Bosco <cristhyandalbosco@gmail.com>
2025-11-28 20:22:48 -0300

    praticando

---
+++ aula013/ambiente.js
@@ -0,0 +1,16 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+// estrutura de repetição com teste lógico no inicio
+*/
+
+var c = 1
+do { 
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+    
+// estrutura de repetição com teste lógico no final
---
+++ aula014/ambiente.js
@@ -0,0 +1,6 @@
+for (var c = 1; c <=10; c++ ) {
+    console.log (`Tentativa de número ${c}`)
+}
+
+
+// simples e direta usada principalmente quando sabemos o limite das execuções
