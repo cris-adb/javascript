b19d265e72846b298819809a33ad5e4a43e5283c
Cristhyan Dal Bosco <cristhyandalbosco@gmail.com>
2025-12-01 20:38:04 -0300

    Update script.js

--- desafios/d003/script.js
+++ desafios/d003/script.js
@@ -1,17 +1,33 @@
 function contar() {
-    let inic = document.querySelector('input#inic')
+    let ini = document.querySelector('input#inic')
     let fim = document.querySelector('input#fim')
     let passo = document.querySelector('input#passo')
-    var res = document.querySelector('div#res')
+    let res = document.querySelector('div#res')
 
-    if  (inic.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+    if  (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
-    }
-
-    for (var i = inic; i <= fim; i += passo) {
-        res.innerHTML += `&#x1F449 ${i}`
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1!')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+            
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
     
 }
